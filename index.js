436b9a38b0fb1b8a1c63cb8167962816f280c9dd
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2023-05-02 01:59:55 +0600

    local api id and all data create first

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port =process.env.PORT || 5000;
 
 const chefiesData = require('./data/chefiesData.json')
+
+app.use(cors());
 
 app.get('/',(req,res)=>{
 res.send('chef is running')
@@ -12,6 +15,12 @@ app.get('/chefiesData',(req,res)=>{
 res.send(chefiesData)
 })
 
+app.get('/chefiesData/:id',(req,res)=>{
+const id = req.params.id;
+console.log(id)
+const selectShef =chefiesData.find(c=>c.id === id)
+res.send(selectShef) 
+})
 
 app.listen(port, ()=>{
 console.log(`chef api is runnig on port: ${port}`)
