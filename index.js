6fc624397afeb1b4176770d5f03a9017f956c30d
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2023-05-02 21:52:16 +0600

    data id create

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ const chefiesData = require('./data/chefiesData.json')
 
 app.use(cors());
 
-// app.get('/',(req,res)=>{
-// res.send('chef is running')
-// })
-
 app.get('/',(req,res)=>{
+res.send('chef is running')
+})
+
+app.get('/chefdata',(req,res)=>{
 res.send(chefiesData)
 })
 
-app.get('/:id',(req,res)=>{
-const id = req.params.id;
+app.get('chefdata/:id',(req,res)=>{
+const id = parseInt(req.params.id);
 console.log(id)
-const selectShef =chefiesData.find(c=>c.id === id)
+const selectShef =chefiesData.find(c=>parent(c.id )=== id)
 res.send(selectShef) 
 })
 
 app.listen(port, ()=>{
-console.log(`chef api is runnig on port: ${port}`)
+console.log(`chef api is running on port: ${port}`)
 })
